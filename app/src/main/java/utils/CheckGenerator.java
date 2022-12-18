a6b3569cb2ffd8c56c306c8004fe468dad62e47d
jskov <jskov259259@rambler.ru>
2022-12-18 14:07:36 +0300

    refactored CheckGenerator

--- app/src/main/java/utils/CheckGenerator.java
+++ app/src/main/java/utils/CheckGenerator.java
@@ -9,26 +9,13 @@ import java.util.Optional;
 
 public class CheckGenerator {
 
+    private static StringBuilder builder = new StringBuilder("Cash Receipt\n");
+
+
     public String generateCheck(Map<Product, Integer> productQuantity, Optional<Card> card) {
-        StringBuilder builder = new StringBuilder("Cash Receipt\n");
+
         builder.append("QTY Description Price TOTAL\n");
-        BigDecimal totalPrice = new BigDecimal("0");
-
-        for (Product product : productQuantity.keySet()) {
-            builder.append(productQuantity.get(product) + " " + product.getDescription() + " " + product.getPrice() + "$ ");
-
-            if (productQuantity.get(product) > 5) {
-                BigDecimal total = product.getPrice().multiply(new BigDecimal(productQuantity.get(product)));
-                BigDecimal discount = total.multiply(new BigDecimal(10)).divide(new BigDecimal(100));
-                total = total.subtract(discount);
-                builder.append(total + "$ (10% discount)\n");
-                totalPrice = totalPrice.add(total);
-            } else {
-                builder.append(product.getPrice().multiply(new BigDecimal(productQuantity.get(product))) + "$\n");
-                totalPrice = totalPrice.add(product.getPrice().multiply(new BigDecimal(productQuantity.get(product))));
-            }
-
-        }
+        BigDecimal totalPrice = calculateTotalPrice(productQuantity);
         builder.append("-------\n");
 
         if (card.isPresent()) {
@@ -37,8 +24,36 @@ public class CheckGenerator {
                     "(" + discount + "$)\n");
             totalPrice = totalPrice.subtract(discount);
         }
-        builder.append("Total: " + totalPrice + "$");
 
+        builder.append("Total: " + totalPrice + "$");
         return builder.toString();
+    }
+
+    BigDecimal calculateTotalPrice(Map<Product, Integer> productQuantity) {
+
+        BigDecimal totalPrice = new BigDecimal("0");
+        totalPrice.setScale(2);
+        for (Product product : productQuantity.keySet()) {
+            Integer quantity = productQuantity.get(product);
+            BigDecimal price = product.getPrice();
+            builder.append(quantity + " " + product.getDescription() + " " + price + "$ ");
+
+            if (quantity > 5) {
+                BigDecimal total = multiplyPriceQuantity(price, quantity);
+                BigDecimal discount = total.multiply(new BigDecimal(10)).divide(new BigDecimal(100));
+                total = total.subtract(discount);
+                builder.append(total + "$ (10% discount)\n");
+                totalPrice = totalPrice.add(total);
+            } else {
+                builder.append(multiplyPriceQuantity(price, quantity) + "$\n");
+                totalPrice = totalPrice.add(price.multiply(new BigDecimal(quantity)));
+            }
+        }
+        return totalPrice;
+    }
+
+    BigDecimal multiplyPriceQuantity(BigDecimal price, Integer quantity) {
+
+        return price.multiply(new BigDecimal(quantity));
     }
 }
