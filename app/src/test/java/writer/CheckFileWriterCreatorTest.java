7bff75be67e3823965aa96931f6a3ed157d8ca6a
jskov <jskov259259@rambler.ru>
2023-02-20 20:00:02 +0300

    test: added tests to writer package

---
+++ app/src/test/java/writer/CheckFileWriterCreatorTest.java
@@ -0,0 +1,24 @@
+package writer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckFileWriterCreatorTest {
+
+    private CheckFileWriterCreator writerCreator;
+
+    @BeforeEach
+    public void setUp() {
+        writerCreator = new CheckFileWriterCreator();
+    }
+
+    @Test
+    public void checkCreateFileWriter() {
+
+        CheckWriter fileWriter = writerCreator.createWriter();
+        assertEquals(fileWriter.getClass(), CheckFileWriter.class);
+    }
+
+}
